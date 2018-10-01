5c25927829156bc7b01345d147f553a50f67e1c9
JC Albano <jcgalbano@gmail.com>
2018-10-01 20:40:04 +0800

    Nest login and register components in new session component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { SessionComponent } from './session/session.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    SessionComponent
   ],
   imports: [
     BrowserModule,
