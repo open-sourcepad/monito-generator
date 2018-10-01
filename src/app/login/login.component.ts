b090f42ead1d9855a287c78fc149c6cd34813b0d
JC Albano <jcgalbano@gmail.com>
2018-10-01 23:11:13 +0800

    Integrate FormBuilder on login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm :any;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.userForm = this.formBuilder.group({
+      'email':[''],
+      'password':['']
+      }
+    )
+  }
+
+  onSubmit(form_params): any {
+    debugger;
+    console.log();
+  }
 
   ngOnInit() {
   }
