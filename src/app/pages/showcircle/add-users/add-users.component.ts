d5962569410c111c6a6be485a9211c19120d536c
Jan Carlo Albano <jcgalbano@gmail.com>
2018-10-15 17:34:08 +0800

    Add many users to circle, Handle validations

--- src/app/pages/showcircle/add-users/add-users.component.ts
+++ src/app/pages/showcircle/add-users/add-users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ControlMessagesComponent } from '../../../control-messages/control-messages.component'
+import { ValidationService } from '../../../control-messages/validation.service'
+
 
 @Component({
   selector: 'app-add-users',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-users.component.css']
 })
 export class AddUsersComponent implements OnInit {
+  usersForm:any;
+  constructor(private formBuilder: FormBuilder) {
+    this.usersForm = this.formBuilder.group({
+      users: this.formBuilder.array([])
+    });
+  }
 
-  constructor() { }
-
+  addUser(){
+    var usersArr = <FormArray>this.usersForm.controls.users;
+    usersArr.push(
+      this.formBuilder.group({
+        'user_name':['',Validators.required],
+        'email':['',[Validators.required, ValidationService.emailValidator]],
+      })
+    )
+    console.log("Add User");
+  };
+  removeUser(i){
+    console.log(i);
+    console.log("Remove User");
+    var usersArr = <FormArray>this.usersForm.controls.users;
+    usersArr.removeAt(i);
+  };
+  onSubmit(formParams){
+    debugger;
+    console.log(formParams);
+  }
   ngOnInit() {
+    this.addUser();
+    console.log(this.usersForm);
+    console.log(this.usersForm.get('users').controls.length);
   }
 
 }
